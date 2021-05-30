0d3373d5b79f83e70e561f262b092945b1295b9a
minhajul <kmsohelinfo@gmail.com>
2021-05-30 23:22:37 +0600

    Updated header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,9 +12,9 @@
 
 <body class="antialiased">
 
-<div class="relative bg-white">
+<div class="relative bg-gray-800">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
-        <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
             <div class="flex justify-start lg:w-0 lg:flex-1">
                 <a href="{{ route('home') }}">
                     <span class="sr-only">Workflow</span>
@@ -44,7 +44,7 @@
 {{--            </nav>--}}
 
             <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                <a href="#" class="whitespace-nowrap text-base font-medium text-white hover:text-gray-300">
                     Sign in
                 </a>
                 <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
