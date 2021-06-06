92cebc00b1e5c49c83d4f80e333d183b3ba357d9
minhajul <kmsohelinfo@gmail.com>
2021-06-06 11:37:08 +0600

    Updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 py-6 lg:py-10">
+    <div class="bg-gray-100">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 py-6 lg:py-10">
 
-        <livewire:blogs-component />
+            <livewire:blogs-component />
 
+        </div>
     </div>
 @endsection
