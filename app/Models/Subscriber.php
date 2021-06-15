1c2621f3d5b2751a58a780b7707f1c41da3e6db7
minhajul <kmsohelinfo@gmail.com>
2021-06-15 22:44:50 +0600

    Updated

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subscriber extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'email',
+        'verified_at'
+    ];
+
+    // Scope
+    public function scopeVerified(Builder $builder): Builder
+    {
+        return $builder->where('verified_at', '!=', null);
+    }
+
+    // Methods
+    public function isVerified(): bool
+    {
+        return $this->verified_at != null;
+    }
 }
