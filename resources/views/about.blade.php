77fb2ed80bddf7ae713906f130410c9e0a42d422
minhajul <kmsohelinfo@gmail.com>
2021-06-21 22:43:30 +0600

    Updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,10 +19,13 @@
                             @if($user->bio)
                                 {!! $user->bio !!}
                             @else
-                                @foreach([1, 2, 3, 4] as $item)
-                                    What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
-                                    <br><br>
-                                @endforeach
+                                What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                                <br><br>
+                                What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                                <br><br>
+                                What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                                <br><br>
+                                What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
                             @endif
                         </p>
                     </div>
