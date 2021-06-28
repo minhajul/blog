100b5b6f3d3a730162ce8ab046f8452236090178
minhajul <kmsohelinfo@gmail.com>
2021-06-28 22:38:18 +0600

    Added seeder

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Contact::factory(50)->create();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             BlogSeeder::class,
-            SubscriberSeeder::class
+            SubscriberSeeder::class,
+            ContactSeeder::class,
         ]);
     }
 }
