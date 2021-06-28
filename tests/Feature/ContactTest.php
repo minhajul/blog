5496a52c558440661a7f1fa1cee746484829ac46
minhajul <kmsohelinfo@gmail.com>
2021-06-28 22:43:04 +0600

    Updated

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContactTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_contact_screen_can_be_rendered()
+    {
+        $response = $this->get(route('contact.index'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_submit_contact()
+    {
+        $response = $this->post(route('contact.store'), [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'message' => 'Test Contact Message',
+        ]);
+
+        $response->assertRedirect();
+        $response->assertSessionHasNoErrors();
+    }
+}
