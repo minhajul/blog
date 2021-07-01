5b3720eeb497f46d9ea28580457b0fc26fc5a6cc
minhajul <kmsohelinfo@gmail.com>
2021-07-01 09:50:24 +0600

    Refactor code

--- app/Http/Controllers/Profile/SettingsController.php
+++ app/Http/Controllers/Profile/SettingsController.php
@@ -21,28 +21,28 @@ class SettingsController extends Controller
     {
         $setting = Setting::first();
 
-        $logoPath = $setting->logo_path ?? null;
-        $faviconPath = $setting->favicon_path ?? null;
+        $request['logo_path'] = $setting->logo_path ?? null;
+        $request['favicon_path'] = $setting->favicon_path ?? null;
 
         if ($request->file('logo')) {
             $logoFileName = Str::random(5) . '.' . $request->file('logo')->extension();
-            $logoPath = $request->file('logo')->storeAs('images', $logoFileName);
+            $request['logo_path'] = $request->file('logo')->storeAs('images', $logoFileName);
         }
 
         if ($request->file('favicon')) {
             $faviconFileName = Str::random(5) . '.' . $request->file('favicon')->extension();
-            $faviconPath = $request->file('favicon')->storeAs('images', $faviconFileName);
+            $request['favicon_path'] = $request->file('favicon')->storeAs('images', $faviconFileName);
         }
 
-        $inputs = [
-            "logo_path" => $logoPath,
-            "favicon_path" => $faviconPath,
-            "site_title" => $request->input('site_title'),
-            "site_description" => $request->input('site_description'),
-            "google_analytics_tracking_id" => $request->input('google_analytics_tracking_id'),
-            "google_analytics_view_id" => $request->input('google_analytics_view_id'),
-            "google_site_verification_code" => $request->input('google_site_verification_code')
-        ];
+        $inputs = $request->only(
+            'logo_path',
+            'favicon_path',
+            'site_title',
+            'site_description',
+            'google_analytics_tracking_id',
+            'google_analytics_view_id',
+            'google_site_verification_code',
+        );
 
         if ($setting) {
             $setting->update($inputs);
