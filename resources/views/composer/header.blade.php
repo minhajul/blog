a8b44217cf9ca0c3899fbb3174db04bb3bd1d51f
minhajul <kmsohelinfo@gmail.com>
2021-07-11 21:24:47 +0600

    Updated

--- resources/views/composer/header.blade.php
+++ resources/views/composer/header.blade.php
@@ -16,6 +16,8 @@
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+<meta name="google-site-verification" content="{{ $setting && $setting->google_site_verification_code ? $setting->google_site_verification_code : "" }}" />
+
 <meta property="description" content="{{ $setting && $setting->site_description ? $setting->site_description : "Simple Blog For Personal Use"  }}">
 <title>
     {{ $setting && $setting->site_title ? $setting->site_title : "Simple Blog"  }}
