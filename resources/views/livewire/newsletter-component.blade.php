f5028866633039b2fcbaee4b80ea6872583fe45c
minhajul <kmsohelinfo@gmail.com>
2021-08-24 23:23:08 +0600

    Updated

--- resources/views/livewire/newsletter-component.blade.php
+++ resources/views/livewire/newsletter-component.blade.php
@@ -18,7 +18,7 @@
                 <input type="email" wire:model.defer="email" id="emailAddress" autocomplete="email" required="" class="block appearance-none min-w-0 w-full bg-white border border-transparent rounded-md py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white focus:border-white focus:placeholder-gray-400 sm:max-w-xs" placeholder="Enter your email">
 
                 <div class="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-                    <button type="submit" class="w-full bg-indigo-500 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">
+                    <button type="submit" class="bg-gradient-to-r from-green-600 to-blue-500 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">
                         Subscribe
                     </button>
                 </div>
