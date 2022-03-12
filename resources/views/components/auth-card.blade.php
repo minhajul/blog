d8286e3bb7aa9c7c23373a561819052d1cc55380
minhajul <kmsohelinfo@gmail.com>
2022-03-12 12:51:29 +0600

    Redesign login card

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,27 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-200">
+    <div class="py-5">
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="w-96 bg-white rounded-3xl mx-auto overflow-hidden shadow-xl">
+        <div class="relative h-48 bg-rose-500 rounded-bl-4xl">
+            <svg class="absolute bottom-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#ffffff" fill-opacity="1" d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,122.7C960,160,1056,224,1152,245.3C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            </svg>
+        </div>
+        <div class="px-10 pt-4 pb-8 bg-white rounded-tr-4xl">
+            {{ $slot }}
+        </div>
     </div>
 </div>
+
+
+{{--<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">--}}
+{{--    <div>--}}
+{{--        {{ $logo }}--}}
+{{--    </div>--}}
+
+{{--    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">--}}
+{{--        {{ $slot }}--}}
+{{--    </div>--}}
+{{--</div>--}}
