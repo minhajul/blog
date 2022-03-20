d9ad5d70005104de8871a0b1b15fe3c5103d5faf
minhajul <kmsohelinfo@gmail.com>
2022-03-20 09:38:09 +0600

    Fixed hover issue

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 @if($setting = \App\Models\Setting::first())
     <img class="h-8 w-auto sm:h-10" src="{{ $setting->logoUrl() }}" alt="Logo">
 @else
-    <h2 class="text-4xl py-2 font-medium text-gray-400 hover:text-gray-500 italic">{{ config('app.name') }}</h2>
+    <h2 class="text-4xl py-2 font-medium text-slate-400 hover:text-slate-500 italic">{{ config('app.name') }}</h2>
 @endif
