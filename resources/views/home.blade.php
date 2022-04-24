07f389694ac05644ae46bfd99634f1a2961f26ed
minhajul <kmsohelinfo@gmail.com>
2022-04-24 10:54:50 +0600

    Updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <livewire:blog.index :viewStyle="$viewStyle"/>
-
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 py-6 lg:py-10">
+        <livewire:blog.index :viewStyle="$viewStyle"/>
+    </div>
 @endsection
-
 
 @push('scripts')
     <script type="text/javascript">
-        window.onscroll = function(event) {
-            console.log(event)
+        window.onscroll = function (event) {
             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                console.log('sss');
                 window.livewire.emit('load-more');
             }
         };
