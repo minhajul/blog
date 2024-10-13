47f82cea42593d1414d03d0ac4f0ad2e91dd46e5
minhajul <kmsohelinfo@gmail.com>
2024-10-13 12:31:54 +0600

    Fixed

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,11 +3,11 @@ import intersect from '@alpinejs/intersect'
 
 window.axios = import('axios');
 
+window._ = import('lodash');
 window.Popper = import('popper.js').default;
 window.$ = import('jquery');
 window.jQuery = import('jquery');
 
 window.trix = import('trix');
 
-// window.Alpine = Alpine
 Alpine.plugin(intersect)
