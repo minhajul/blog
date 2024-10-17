2a3fe7323413db940335f95116c8e8c66113d297
minhajul <kmsohelinfo@gmail.com>
2024-10-17 15:59:38 +0600

    Fixed missing component issue

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -3,29 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@tailwindcss">
-    {{--<meta name="twitter:title" content="{{ $setting && $setting->site_title ? $setting->site_title : "Minimal Blog For Personal Use"  }}">--}}
-    {{--<meta name="twitter:description" content="{{ $setting && $setting->site_description ? $setting->site_description : "Minimal Blog For Personal Use"  }}">--}}
-    {{--<meta name="twitter:image" content="{{ $setting && $setting->logoUrl() ? $setting->logoUrl() : '#' }}">--}}
     <meta name="twitter:creator" content="@">
-
     <meta property="og:url" content="{{ route('home') }}">
     <meta property="og:type" content="blog">
-    {{--<meta property="og:title" content="{{ $setting && $setting->site_title ? $setting->site_title : "Minimal Blog For Personal Use"  }}">--}}
-    {{--<meta property="og:description" content="{{ $setting && $setting->site_description ? $setting->site_description : "Minimal Blog For Personal Use"  }}">--}}
-    {{--<meta property="og:image" content="{{ $setting && $setting->logoUrl() ? $setting->logoUrl() : '#' }}">--}}
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    {{--<meta name="google-site-verification" content="{{ $setting && $setting->google_site_verification_code ? $setting->google_site_verification_code : "" }}" />--}}
-
-    {{--<meta property="description" content="{{ $setting && $setting->site_description ? $setting->site_description : "Minimal Blog For Personal Use"  }}">--}}
-    <title>
-        {{--    {{ $setting && $setting->site_title ? $setting->site_title : "Minimal Blog For Personal Use"  }} - {{ config('app.name') }}--}}
-        Minimal Blog For Personal Use - {{ config('app.name') }}
-    </title>
+    <title>Minimal Blog For Personal Use - {{ config('app.name') }}</title>
 
     @livewireStyles
 
--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -1,4 +1,3 @@
-
 @if ($errors->any())
     <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
         <span class="block sm:inline"><strong>Whoops!</strong> There were some problems with your input.<br></span>
