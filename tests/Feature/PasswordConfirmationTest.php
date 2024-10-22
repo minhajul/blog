a8e88648f7642fc23bc5e094602b1611f5776095
gh-actions <gh@example.com>
2024-10-22 17:27:58 +0000

    Laravel Pint

--- tests/Feature/PasswordConfirmationTest.php
+++ tests/Feature/PasswordConfirmationTest.php
@@ -11,14 +11,14 @@ class PasswordConfirmationTest extends TestCase
 {
     use RefreshDatabase;
 
-//    public function test_confirm_password_screen_can_be_rendered()
-//    {
-//        $user = User::factory()->create();
-//
-//        $response = $this->actingAs($user)->get('/confirm-password');
-//
-//        $response->assertStatus(200);
-//    }
+    //    public function test_confirm_password_screen_can_be_rendered()
+    //    {
+    //        $user = User::factory()->create();
+    //
+    //        $response = $this->actingAs($user)->get('/confirm-password');
+    //
+    //        $response->assertStatus(200);
+    //    }
 
     /**
      * @throws JsonException
@@ -35,14 +35,14 @@ class PasswordConfirmationTest extends TestCase
         $response->assertSessionHasNoErrors();
     }
 
-//    public function test_password_is_not_confirmed_with_invalid_password()
-//    {
-//        $user = User::factory()->create();
-//
-//        $response = $this->actingAs($user)->post('/confirm-password', [
-//            'password' => 'wrong-password',
-//        ]);
-//
-//        $response->assertSessionHasErrors();
-//    }
+    //    public function test_password_is_not_confirmed_with_invalid_password()
+    //    {
+    //        $user = User::factory()->create();
+    //
+    //        $response = $this->actingAs($user)->post('/confirm-password', [
+    //            'password' => 'wrong-password',
+    //        ]);
+    //
+    //        $response->assertSessionHasErrors();
+    //    }
 }
