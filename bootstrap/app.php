602102a57ca3c96ec6b645f4a7d24a674323732d
minhajul <kmsohelinfo@gmail.com>
2024-10-23 01:02:14 +0600

    Updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,11 +9,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-        then: function () {
-            Route::middleware(['guest', 'web'])
-                ->group(base_path('routes/auth.php'));
-        }
+        health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::group(['middleware' => 'auth'], function () {
 
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+require __DIR__.'/auth.php';
