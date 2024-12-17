d5835e7bcd8cbdadc76235a4a12668233e86263f
minhajul <kmsohelinfo@gmail.com>
2024-12-17 09:51:03 +0600

    Updated

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+use App\Console\Commands\CreateSuperUser;
+use App\Console\Commands\RecoverAdminAccess;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+//Artisan::command('admin:create-user', function () {
+//    $this->comment(CreateSuperUser::class);
+//})->purpose('Create super user');
+//
+//Artisan::command('admin:recover-access', function () {
+//    $this->comment(RecoverAdminAccess::class);
+//})->purpose('Recover admin access if you can not remember the password.');
