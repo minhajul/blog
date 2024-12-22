17dda9bab7b69d8c62792c7d450b523cde567a4a
minhajul <kmsohelinfo@gmail.com>
2024-12-22 21:13:17 +0600

    Updated console

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,6 +22,7 @@ class HomeController extends Controller
             return view('show', compact('blog'));
         }
 
+        
         $viewStyle = $this->getViewStyle();
 
         return view('home', compact('viewStyle'));
--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,2 @@
 <?php
 
-use App\Console\Commands\CreateSuperUser;
-use App\Console\Commands\RecoverAdminAccess;
-use Illuminate\Support\Facades\Artisan;
-
-//Artisan::command('admin:create-user', function () {
-//    $this->comment(CreateSuperUser::class);
-//})->purpose('Create super user');
-//
-//Artisan::command('admin:recover-access', function () {
-//    $this->comment(RecoverAdminAccess::class);
-//})->purpose('Recover admin access if you can not remember the password.');
