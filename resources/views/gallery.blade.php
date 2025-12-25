0b0dcef7cf2b06d1b22e875a4f3b3ca34f7311c2
Md Minhajul Islam <kmsohelinfo@gmail.com>
2025-12-26 00:25:37 +0600

    Updated

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,31 +1,6 @@
 <x-app-layout>
     <div class="py-6 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-10">
         <div class="relative max-w-4xl mx-auto">
-
-            <svg class="absolute left-full transform translate-x-1/2" width="404" height="404" fill="none"
-                 viewBox="0 0 404 404" aria-hidden="true">
-                <defs>
-                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20"
-                             patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200 dark:text-gray-600"
-                              fill="currentColor"></rect>
-                    </pattern>
-                </defs>
-                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"></rect>
-            </svg>
-
-            <svg class="absolute right-full bottom-0 transform -translate-x-1/2" width="404" height="404" fill="none"
-                 viewBox="0 0 404 404" aria-hidden="true">
-                <defs>
-                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20"
-                             patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200 dark:text-gray-600"
-                              fill="currentColor"></rect>
-                    </pattern>
-                </defs>
-                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"></rect>
-            </svg>
-
             <div class="text-center">
                 <h2 x-intersect="$el.classList.add('fadeInUp')"
                     class="text-3xl font-extrabold tracking-tight text-slate-800 dark:text-slate-400 sm:text-4xl">
@@ -38,15 +13,15 @@
                 </p>
             </div>
 
-            <ul role="list"
-                class="py-10 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-6 xl:gap-x-8">
-
+            <ul role="list" class="py-10 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-6 xl:gap-x-8">
                 @forelse($galleries as $gallery)
                     <li class="relative">
-                        <div
-                            class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+                        <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
                             <img class="object-cover pointer-events-none group-hover:opacity-75"
-                                 x-intersect="$el.classList.add('scale')" src="{{ $gallery->image_url }}" alt="">
+                                 x-intersect="$el.classList.add('scale')"
+                                 src="{{ $gallery->image_url }}"
+                                 alt=""
+                            >
                             <button type="button" class="absolute inset-0 focus:outline-none">
                                 <span class="sr-only">{{ $gallery->id }}</span>
                             </button>
@@ -57,7 +32,6 @@
                         class="mt-4 text-lg leading-6 text-slate-500 dark:text-slate-400">No gallery added
                     </li>
                 @endforelse
-
             </ul>
         </div>
     </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,29 +1,6 @@
 <x-app-layout>
     <div class="py-6 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-10">
         <div class="relative max-w-3xl mx-auto">
-
-            <svg class="absolute left-full transform translate-x-1/2" width="404" height="404" fill="none"
-                 viewBox="0 0 404 404" aria-hidden="true">
-                <defs>
-                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20"
-                             patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-600" fill="currentColor"></rect>
-                    </pattern>
-                </defs>
-                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"></rect>
-            </svg>
-
-            <svg class="absolute right-full bottom-0 transform -translate-x-1/2" width="404" height="404" fill="none"
-                 viewBox="0 0 404 404" aria-hidden="true">
-                <defs>
-                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20"
-                             patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-600" fill="currentColor"></rect>
-                    </pattern>
-                </defs>
-                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"></rect>
-            </svg>
-
             <div class="mt-5">
                 <div class="relative px-4 sm:px-6 lg:px-8">
                     <div class="text-lg max-w-prose mx-auto">
@@ -33,14 +10,11 @@
                     </div>
 
                     <div class="mt-6 prose prose-indigo prose-lg text-slate-500 dark:text-slate-400 mx-auto">
-
                         {!! $blog->details !!}
-
                     </div>
 
                     <div class="mt-10">
-                        <a href="{{ route('home') }}"
-                           class="bg-gradient-to-r from-blue-600 to-indigo-500 rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2">
+                        <a href="{{ route('home') }}" class="bg-gradient-to-r from-blue-600 to-indigo-500 rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2">
                             Back to Blog
                         </a>
                     </div>
