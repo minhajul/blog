b6ed122f8f1df410aea23159413f70b70d09a9a6
Md Minhajul Islam <kmsohelinfo@gmail.com>
2025-12-27 22:02:46 +0600

    Updated

--- resources/views/livewire/blog/profile.blade.php
+++ resources/views/livewire/blog/profile.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="border-b border-gray-200">
         <nav class="-mb-px flex content-center space-x-8" aria-label="Tabs">
-            @foreach(\App\Enums\BlogStatus::cases() as $item)
+            @foreach($statuses as $item)
                 <p wire:click="filterByStatus('{{ $item }}')" class="cursor-pointer border-transparent text-gray-500 {{ $status == $item ? "border-indigo-500 text-indigo-600" : "hover:text-gray-700 hover:border-gray-300" }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
                     {{ ucfirst($item->value) }}
                 </p>
@@ -10,16 +10,20 @@
     </div>
 
     <div class="my-6">
-        <flux:input type="text" wire:model.live.debounce.300ms="keywords" placeholder="Search blogs" />
+        <flux:input
+            type="text"
+            wire:model.live.debounce.300ms="keywords"
+            placeholder="Search blogs"
+        />
     </div>
 
     <div class="max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
         @forelse($blogs as $blog)
-            <div class="mb-5 flex flex-col rounded-md shadow-md overflow-hidden">
+            <div class="mb-4 flex flex-col rounded-md shadow-md overflow-hidden">
                 <div class="shrink-0">
                     <img class="h-48 w-full object-cover" src="{{ $blog->bannerUrl() }}" alt="Banner">
                 </div>
-                <div class="flex-1 bg-white p-5 flex flex-col justify-between">
+                <div class="flex-1 bg-white p-4 flex flex-col justify-between">
                     <div class="flex-1">
                         <a href="{{ route('dashboard.blogs.show', $blog) }}" class="block">
                             <p class="text-xl font-semibold text-gray-900">
@@ -31,15 +35,20 @@
                         </a>
                     </div>
 
-                    <div class="mt-5 flex justify-between  text-sm text-gray-500">
-                        <div>
-                            <time datetime="2020-03-16">
-                                Posted {{ $blog->created_at->diffForHumans() }}
-                            </time>
-                        </div>
+                    <div class="mt-4 flex justify-between  text-sm text-gray-500">
+                        <time datetime="{{ $blog->created_at }}">
+                            Posted {{ $blog->created_at->diffForHumans() }}
+                        </time>
 
                         @if(!$blog->isArchived())
-                            <a onclick="return confirm('Are you sure?')" href="{{ route('dashboard.blogs.archived', $blog) }}" class="order-last text-xs bg-red-500 text-white p-1 rounded-md">Mark as Archive</a>
+                            <flux:button
+                                href="{{ route('dashboard.blogs.archived', $blog) }}"
+                                variant="danger"
+                                size="xs"
+                                onclick="return confirm('Are you sure?')"
+                            >
+                                Mark as Archive
+                            </flux:button>
                         @endif
                     </div>
                 </div>
