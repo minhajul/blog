9e0357c07a37fbda66110b7eec4334d1b1d7c6b8
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-01-04 23:03:18 +0600

    Updated

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use RefreshDatabase;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->withoutVite();
-    }
+    //
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-final class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+it('returns a successful response', function () {
+    $this->get(route('home'))
+        ->assertStatus(200);
+});
