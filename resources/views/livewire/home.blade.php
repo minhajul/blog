2cfecd0011d22fbe551691af986656436dc9e7fa
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-01-17 14:37:43 +0600

    Updated

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,8 +1,8 @@
 <div class="max-w-7xl mx-auto px-4 sm:px-6 py-6 lg:py-10">
-    <div class="max-w-2xl mx-auto text-center py-10 px-4 sm:py-16 sm:px-6 lg:px-8">
+    <div class="max-w-2xl mx-auto text-center py-6 px-4 sm:py-10 sm:px-6 lg:px-8">
         <div class="grid place-items-center">
-            <h2 class="mb-5 animate-move-bg bg-gradient-to-r from-indigo-500 via-pink-500 to-indigo-500 bg-[length:400%] bg-clip-text text-3xl font-extrabold sm:text-4xl font-bold text-transparent">
-                Search from blog now.
+            <h2 class="mb-5 animate-move-bg bg-gradient-to-r from-indigo-500 via-pink-500 to-indigo-500 bg-[length:400%] bg-clip-text text-3xl lg:text-4xl font-bold text-transparent">
+                Search From Blog
             </h2>
         </div>
 
