ad8df81bffe35acc75081f875745170e734cd2d1
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-04-12 22:50:43 +0600

    updated

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -1,9 +1,9 @@
 <div class="flex items-start max-md:flex-col">
-    <div class="me-10 w-full pb-4 md:w-[220px] flex-shrink-0 pt-6">
+    <div class="me-10 w-full pb-4 md:w-55 shrink-0 pt-6">
         <flux:navlist>
             <flux:navlist.item :href="route('dashboard')" icon="home" wire:navigate>{{ __('Home') }}</flux:navlist.item>
             <flux:navlist.item :href="route('dashboard.blogs.index')" icon="pencil-square" wire:navigate>{{ __('Blogs') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('dashboard.projects.index')" icon="clipboard-document-list" wire:navigate>{{ __('Project') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('dashboard.projects.index')" icon="clipboard-document-list" wire:navigate>{{ __('Projects') }}</flux:navlist.item>
             <flux:navlist.item :href="route('dashboard.experiences.index')" icon="briefcase" wire:navigate>{{ __('Experiences') }}</flux:navlist.item>
             <flux:navlist.item :href="route('subscribers.index')" icon="chart-bar-square" wire:navigate>{{ __('Subscribers') }}</flux:navlist.item>
             <flux:navlist.item :href="route('contacts.index')" icon="users" wire:navigate>{{ __('Contacts') }}</flux:navlist.item>
